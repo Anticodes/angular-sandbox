03c2435e5ab8d20b20889a4c59ede7c031ccb34f
Anticodes <orkunmeral7@gmail.com>
2023-07-31 13:48:47 +0300

    feat: :sparkles: implement orbiting text input value and set a size

--- src/app/orbiting-text/orbiting-text.component.ts
+++ src/app/orbiting-text/orbiting-text.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-orbiting-text',
-  templateUrl: './orbiting-text.component.html',
-  styleUrls: ['./orbiting-text.component.scss']
+    selector: 'orbiting-text',
+    templateUrl: './orbiting-text.component.html',
+    styleUrls: ['./orbiting-text.component.scss']
 })
 export class OrbitingTextComponent {
-
+    @Input() text: string = "";
 }
