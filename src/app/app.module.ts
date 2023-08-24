a108a689ea06307e22ca08ffa87a36d7d2ece55f
Anticodes <orkunmeral7@gmail.com>
2023-08-25 00:19:47 +0300

    feat: :construction: stepper component in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OdometerInputComponent } from './components/odometer-input/odometer-input.component';
 import { OrbitingTextComponent } from './components/orbiting-text/orbiting-text.component';
+import { StepperComponent } from './components/stepper/stepper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OdometerInputComponent,
     OrbitingTextComponent,
+    StepperComponent,
   ],
   imports: [
     BrowserModule,
